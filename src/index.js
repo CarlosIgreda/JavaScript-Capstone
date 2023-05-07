8bc940d509089a023b69909c9cad84f0c4fc5122
CarlosIgreda <cjigredarivas@gmail.com>
2023-05-06 21:50:42 -0500

    likes and comments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,67 @@
+import './index.css';
+
+import countLikes from './modules/countLikes.js';
+
 document.addEventListener('DOMContentLoaded', () => {
   const $movieCards = document.querySelector('.movie-cards');
 
   let movies = [];
+  let comments = [];
   const API_URL = 'https://api.themoviedb.org/3/trending/movie/day?api_key=e69f1af4101d7b0a6dc2c4def7adf831';
   const IMAGE_PATH = 'https://image.tmdb.org/t/p/original/';
+  const INV_API_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/HLTRIFVQwjKB70lwmVHy/';
+
+  const addLike = async (id) => {
+    try {
+      await fetch(INV_API_URL.concat('likes'), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify({
+          item_id: id,
+        }),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addComment = async (id, user, comment) => {
+    await fetch(INV_API_URL.concat('comments'), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        item_id: id,
+        username: user.trim(),
+        comment: comment.trim(),
+      }),
+    });
+  };
+
+  const getLikes = async (url, movieid) => {
+    try {
+      const response = await fetch(url);
+      const dataLikes = await response.json();
+      const like = dataLikes.find((like) => like.item_id === movieid);
+      return like.likes;
+    } catch {
+      return 0;
+    }
+  };
+
+  const getComments = async (url) => {
+    const response = await fetch(url);
+    const dataComments = await response.json();
+    return dataComments;
+  };
 
   const renderData = () => {
     const $countMovies = document.querySelector('.count-movies');
-    $countMovies.textContent += ` (${movies.length})`;
+    countLikes($countMovies, movies.length);
+    const $numberComments = document.querySelector('.number-comments');
     movies.forEach(async (movie) => {
       const $card = document.createElement('div');
       $card.setAttribute('class', 'card-container');
@@ -49,6 +103,85 @@ document.addEventListener('DOMContentLoaded', () => {
       $infoContainer.appendChild($textContainer);
       $infoContainer.appendChild($commentLikeContainer);
       $card.appendChild($infoContainer);
+      $likeIconRegular.addEventListener('click', async () => {
+        $likeIconRegular.style.display = 'none';
+        $likeIconSolid.style.display = 'block';
+        setTimeout(() => {
+          $likeIconSolid.style.display = 'none';
+          $likeIconRegular.style.display = 'block';
+        }, 1000);
+        await addLike(movie.id);
+        $likeText.textContent = await getLikes(INV_API_URL.concat('likes'), movie.id);
+      });
+      $likeText.textContent = await getLikes(INV_API_URL.concat('likes'), movie.id);
+      const $popUpContainer = document.querySelector('.popup-container');
+      const $commentsTable = document.querySelector('.comments-table');
+      $commentButton.addEventListener('click', async () => {
+        console.log(movie.id);
+        comments = JSON.parse(localStorage.getItem(`comments_${movie.id}`)) || [];
+        const $popUpMovieTitle = document.querySelector('.popup-movie-title');
+        $popUpMovieTitle.textContent = movie.title;
+        const $popUpMovieBackdrop = document.querySelector('.popup-movie-backdrop');
+        $popUpMovieBackdrop.setAttribute('src', IMAGE_PATH.concat(movie.backdrop_path));
+        const $popUpMovieOverview = document.querySelector('.popup-movie-overview-text');
+        $popUpMovieOverview.textContent = ` ${movie.overview}`;
+        const $popUpMovieRelease = document.querySelector('.popup-movie-release-text');
+        $popUpMovieRelease.textContent = ` ${movie.release_date}`;
+        $popUpContainer.style.display = 'flex';
+
+        // Obtenemos los comentarios existentes
+        comments = await getComments(INV_API_URL.concat(`comments?item_id=${movie.id}`));
+
+        if (comments.length > 0) {
+          // Si hay comentarios, los agregamos al contenedor
+          $commentsTable.innerHTML = '';
+          comments.forEach((comment) => {
+            const $commentItem = document.createElement('li');
+            $commentItem.setAttribute('class', 'comment-item');
+            $commentItem.textContent = `[${comment.creation_date}] ${comment.username} - ${comment.comment}`;
+            $commentsTable.appendChild($commentItem);
+          });
+          $numberComments.textContent = ` (${comments.length})`;
+        } else {
+          // Si no hay comentarios, mostramos un mensaje indicando que no hay comentarios
+          $commentsTable.innerHTML = '<li>No hay comentarios para esta película.</li>';
+          $numberComments.textContent = '';
+        }
+
+        const $addCommentButton = document.querySelector('.popup-comment-button');
+        $addCommentButton.addEventListener('click', async () => {
+          console.log(movie.id);
+          const $inputName = document.querySelector('.input-name');
+          const $inputComment = document.querySelector('.input-comment');
+          if ($inputName.value.trim() && $inputComment.value.trim()) {
+            await addComment(movie.id, $inputName.value, $inputComment.value);
+            $inputName.value = '';
+            $inputComment.value = '';
+
+            // Actualizamos los comentarios después de agregar uno nuevo
+            comments = await getComments(INV_API_URL.concat(`comments?item_id=${movie.id}`));
+            console.log(movie.id);
+            $commentsTable.innerHTML = '';
+            comments.forEach((comment) => {
+              const $commentItem = document.createElement('li');
+              $commentItem.setAttribute('class', 'comment-item');
+              $commentItem.textContent = `[${comment.creation_date}] ${comment.username} - ${comment.comment}`;
+              $commentsTable.appendChild($commentItem);
+            });
+            $numberComments.textContent = ` (${comments.length})`;
+          } else {
+            $inputName.value = '';
+            $inputComment.value = '';
+          }
+        });
+        const $xIcon = document.querySelector('.x-icon');
+        $xIcon.addEventListener('click', () => {
+          $popUpContainer.style.display = 'none';
+          $numberComments.textContent = '';
+          $commentsTable.innerHTML = '';
+          comments = [];
+        });
+      });
     });
   };
 
